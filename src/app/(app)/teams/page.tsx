afbfaa2ccd702a156a912af998fb4fac37ea4577
Teon Anderson <isystemsdirect@gmail.com>
2025-10-14 05:19:36 +0000

    Try fixing this error: `Runtime Error: ReferenceError: CardFooter is not

--- src/app/(app)/teams/page.tsx
+++ src/app/(app)/teams/page.tsx
@@ -1,3 +1,4 @@
+
 import {
   MoreHorizontal,
   PlusCircle,
@@ -13,6 +14,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
