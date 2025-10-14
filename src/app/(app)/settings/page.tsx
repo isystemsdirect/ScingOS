1e7e683a5a7c8f061188671b4df050fa060f3074
Teon Anderson <isystemsdirect@gmail.com>
2025-10-14 04:04:34 +0000

    Try fixing this error: `Runtime Error: ReferenceError: CardFooter is not

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { CircleUser, Home, LineChart, Package, Package2, PanelLeft, Search, Settings, ShoppingCart, Users2 } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
