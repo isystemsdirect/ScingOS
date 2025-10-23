834b1f921f17e5500c44210496829dbcd1aa7d85
Your Name <isystemsdirect@gmail.com>
2025-10-23 17:43:52 +0000

    I do not want the sidebar to move when scrolling the page

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -107,7 +107,7 @@ export default function AppLayout({
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] relative">
       <BackgroundSlideshow />
-      <div className="hidden border-r bg-sidebar/90 md:block z-10">
+      <div className="hidden border-r bg-sidebar/80 md:block z-10">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Logo />
@@ -269,9 +269,11 @@ export default function AppLayout({
               <FullscreenToggle />
           </div>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-transparent">
-          {children}
-        </main>
+        <div className="flex-1 overflow-auto">
+            <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-transparent">
+              {children}
+            </main>
+        </div>
       </div>
     </div>
   );
