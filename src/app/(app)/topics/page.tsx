4fe9997c3c1d1b550baae1112210d95c7e26121e
Teon Anderson <isystemsdirect@gmail.com>
2025-10-30 22:27:28 +0000

    Okay so the "Topics" page architecture should relate to a page that has

--- src/app/(app)/topics/page.tsx
+++ src/app/(app)/topics/page.tsx
@@ -52,7 +52,7 @@ export default function TopicsPage() {
           </div>
           <div className="ml-auto flex items-center gap-2">
             <Button size="sm" variant="outline" className="h-8 gap-1" asChild>
-                <Link href="/community">
+                <Link href="/social">
                     <Rss className="h-3.5 w-3.5" />
                     <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                         View Timeline
