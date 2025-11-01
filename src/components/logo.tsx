feea89fa2f8723014236c888df56ce0eec0d972b
Teon Anderson <hummingbirdhumz@gmail.com>
2025-11-01 00:14:16 +0000

    move "TM" 2 spaces to the right

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,3 +1,4 @@
+
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
@@ -16,10 +17,10 @@ const Logo = ({ className, isLoginPage = false }: { className?: string, isLoginP
             <Image src="/logo.png" alt="Scingular Logo" width={192} height={192} className="size-48 relative" />
         </div>
         <div className="relative flex items-baseline justify-center">
-          <span className="font-sans uppercase font-bold italic text-3xl whitespace-nowrap">
+          <span className="font-sans uppercase font-bold italic text-5xl whitespace-nowrap">
             SCINGULAR <span className="text-primary">AI</span>
           </span>
-          <span className="font-sans font-bold text-[0.4em] absolute top-[0.1em] -right-[1.4em]">TM</span>
+          <span className="font-sans font-bold text-[0.4em] absolute top-[0.1em] -right-[2.4em]">TM</span>
         </div>
       </Link>
     );
