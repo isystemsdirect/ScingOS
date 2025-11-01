f80039244b636c32237f03380b557dbd30e3be9d
Teon Anderson <hummingbirdhumz@gmail.com>
2025-11-01 00:51:47 +0000

    make 3 font points bigger (_for element span_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -18,10 +18,10 @@ const Logo = ({ className, isLoginPage = false }: { className?: string, isLoginP
             <Image src="/logo.png" alt="Scingular Logo" width={192} height={192} className="size-32 sm:size-40 md:size-48 relative" />
         </div>
         <div className="relative flex items-baseline justify-center">
-           <span className="font-sans uppercase font-bold italic text-2xl sm:text-3xl md:text-2xl whitespace-nowrap">
-            <span className="text-scingular-glow">SCINGULAR</span> <span className="text-primary text-ai-glow">AI</span>
+           <span className="font-sans uppercase font-bold italic text-2xl sm:text-3xl md:text-4xl whitespace-nowrap">
+            <span className="text-scingular-glow">SCINGULAR</span> <span className="text-primary text-ai-glow text-3xl sm:text-4xl md:text-5xl">AI</span>
           </span>
-          <span className="font-sans font-bold text-[0.4em] absolute top-[0.6em] right-[-4.2em] sm:top-[0.8em] sm:-right-[4.2em] md:top-[0.6em] md:-right-[4.2em]">TM</span>
+          <span className="font-sans font-bold text-[0.4em] absolute top-[0.6em] right-[-4.2em] sm:top-[0.8em] sm:-right-[4.2em] md:top-[0.6em] md:-right-[3.2em]">TM</span>
         </div>
       </Link>
     );
