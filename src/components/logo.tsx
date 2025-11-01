51666e380f2315ee3fa3c7a555add673d9646011
Teon Anderson <hummingbirdhumz@gmail.com>
2025-11-01 00:15:32 +0000

    And, I want "SCINGULAR AI" to be glowing dark blue

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -17,10 +17,10 @@ const Logo = ({ className, isLoginPage = false }: { className?: string, isLoginP
             <Image src="/logo.png" alt="Scingular Logo" width={192} height={192} className="size-48 relative" />
         </div>
         <div className="relative flex items-baseline justify-center">
-          <span className="font-sans uppercase font-bold italic text-5xl whitespace-nowrap">
+           <span className="font-sans uppercase font-bold italic text-4xl whitespace-nowrap text-glow-blue">
             SCINGULAR <span className="text-primary">AI</span>
           </span>
-          <span className="font-sans font-bold text-[0.4em] absolute top-[0.1em] -right-[2.4em]">TM</span>
+          <span className="font-sans font-bold text-[0.4em] absolute top-[0.8em] -right-[2.5em]">TM</span>
         </div>
       </Link>
     );
