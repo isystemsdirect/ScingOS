db4bc609eef74300c6407575c9681cea7ba6095d
Teon Anderson <isystemsdirect@gmail.com>
2025-12-03 23:18:20 +0000

    what happened to logo?

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,15 +9,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "interactive-pulse bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-primary/10 hover:text-primary",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-primary/10 hover:text-primary",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        pro: "bg-pro text-pro-foreground hover:bg-pro/90",
       },
       size: {
         default: "h-10 px-4 py-2",
